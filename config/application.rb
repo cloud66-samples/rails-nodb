e8af32ebf2899b688a334afadc046b600bb8a627
Philip Källberg <philip@cloud66.com>
2014-01-30 14:21:33 +0000

    Update application.rb

--- config/application.rb
+++ config/application.rb
@@ -16,7 +16,6 @@ module RailsNodb
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-    config.i18n.available_locales = ENV['AVAILABLE_LOCALES'].split(' | ')
     
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
