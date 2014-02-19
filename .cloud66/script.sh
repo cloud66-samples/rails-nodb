080390ba54f48f5de092443a11cb922c4c666325
Philip Källberg <philip@cloud66.com>
2014-02-19 11:10:52 +0000

    Update script.sh

--- .cloud66/script.sh
+++ .cloud66/script.sh
@@ -1,3 +1,4 @@
 mkdir -p $STACK_BASE/shared/uploads
 chown nginx:app_writers $STACK_BASE/shared/uploads
+rm -rf $STACK_PATH/public/uploads
 ln -nsf $STACK_BASE/shared/uploads $STACK_PATH/public/uploads
