56cd25e755027a23915275a36dbfaf6f15720619
Philip Källberg <philip@cloud66.com>
2014-02-20 10:42:36 +0000

    Update script.sh

--- .cloud66/script.sh
+++ .cloud66/script.sh
@@ -1,4 +1,3 @@
 mkdir -p $STACK_BASE/shared/uploads/course/handout
 chown nginx:app_writers $STACK_BASE/shared/uploads/course/handout
-mkdir -p $STACK_PATH/public/uploads/course/handout
 ln -nsf $STACK_BASE/shared/uploads/course/handout $STACK_PATH/public/uploads/course/handout
