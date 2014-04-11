d3104893afa2d19c4e8f44b863a5963320bbe058
Philip Källberg <philip@cloud66.com>
2014-04-11 10:48:43 +0200

    Update schedule.rb

--- config/schedule.rb
+++ config/schedule.rb
@@ -2,4 +2,8 @@ env :PATH, ENV['PATH']
 
 every 2.minutes do
   command "ENV['STACK_BASE'] >> /tmp/tja"
+  command "ENV['STACK_PATH'] >> /tmp/tja"
+  command "test3 >> /tmp/tja"
+  command "$STACK_BASE >> /tmp/tja"
+  command "$STACK_PATH >> /tmp/tja"
 end
