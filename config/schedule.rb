cb5a2861a39d6c8771c9d3a707a1cab9c55f6964
lvangool <luvic.vangool@gmail.com>
2014-04-11 12:02:26 +0100

    Update schedule.rb

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,9 +1,9 @@
 env :PATH, ENV['PATH']
 
 every 2.minutes do
-  command "ENV['STACK_BASE'] >> /tmp/tja"
-  command "ENV['STACK_PATH'] >> /tmp/tja"
-  command "test3 >> /tmp/tja"
-  command "$STACK_BASE >> /tmp/tja"
-  command "$STACK_PATH >> /tmp/tja"
+  command "echo ENV['STACK_BASE'] >> /tmp/tja1"
+  command "echo ENV['STACK_PATH'] >> /tmp/tja2"
+  command "echo 'test3' >> /tmp/tja3"
+  command "echo $STACK_BASE >> /tmp/tja4"
+  command "echo $STACK_PATH >> /tmp/tja5"
 end
