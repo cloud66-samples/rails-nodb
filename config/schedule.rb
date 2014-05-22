2ce566a9572d31f681a44c416c7a907bd87120c2
Philip Källberg <philip@cloud66.com>
2014-05-22 17:28:00 +0100

    Update schedule.rb

--- config/schedule.rb
+++ config/schedule.rb
@@ -8,30 +8,12 @@
 #  command "echo $STACK_PATH >> /tmp/tja5"
 #end
 
-require 'active_support/all'
-
 env :PATH, ENV['PATH']
-
-def eastern(x)
-  ActiveSupport::TimeWithZone.new(nil, ActiveSupport::TimeZone['Eastern Time (US & Canada)'], DateTime.parse(x)).utc
-end
 
 if ENV['PRIMARY'] == 'true'
   if ENV['RAILS_ENV'] == 'production'
     every 4.hours, roles: [:app] do
-      command "pwd"
+      rake "bundle exec rake test:work"
     end
-  end
-
-  every :day, at: eastern('1:00pm'), roles: [:app] do
-    command "pwd"
-  end
-
-  every :day, at: eastern('5:00pm'), roles: [:app] do
-    command "pwd"
-  end
-
-  every :day, at: eastern('5:00pm'), roles: [:app] do
-    command "pwd"
   end
 end
